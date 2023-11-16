949e9698bf538306ea2f0b56c1949e45f70ee1a9
Haibt9702 <buihaibq9702@gmail.com>
2023-11-16 18:33:55 +0700

    mock data into mongoDB

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ import generalRoutes from './routes/general.js';
 import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 
+// data imports
+import User from "./models/User.js";
+import {
+    dataUser,
+} from "./data/index.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -37,5 +42,8 @@ mongoose
     })
     .then(() => {
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+        /* ONLY ADD DATA ONE TIME */
+        //User.insertMany(dataUser);
     })
     .catch((error) => console.log(`${error} did not connect`));
